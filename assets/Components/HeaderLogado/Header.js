8b790f7fca84776c3c8a873d90a6fba134c8fee2
Nathan Viana <nathanviana2007@hotmail.com>
2026-03-13 19:08:03 -0300

    Tela de Editar Perfil feita

--- assets/Components/HeaderLogado/Header.js
+++ assets/Components/HeaderLogado/Header.js
@@ -20,10 +20,10 @@ export class MeuHeader extends HTMLElement {
                             <a class="nav-link text-dark" href="#">GAME PLUS +</a>
                             <a class="nav-link text-dark" href="#">UNIDADES</a>
                             <a class="nav-link text-dark" href="#">CONTATO</a>
-                            <a class="btn-perfil-sair" href="login.html">
+                            <a class="btn-perfil-sair" href="../../Pages/EditarPerfil/EditarPerfil.html">
                                 <span class="texto-sair">SAIR</span>
                                 <div class="circulo-azul">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" >
                                         <path fill="white"
                                             d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512h388.6c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                                     </svg>
