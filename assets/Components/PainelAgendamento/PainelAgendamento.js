19ccf8d65e8f6ad9874921c5e95d58967c839935
Cleuber Pacheco <profcleuberjr@gmail.com>
2026-05-18 20:41:18 -0300

    botao voltar

--- assets/Components/PainelAgendamento/PainelAgendamento.js
+++ assets/Components/PainelAgendamento/PainelAgendamento.js
@@ -20,6 +20,11 @@ export class PainelAgendamento extends HTMLElement {
     render() {
         this.innerHTML = `
         <div class="agendamento-container">
+            <div class="row">
+                <div class=" col-12 col-md-4">
+                    <botao-voltar texto="&larr; Voltar" href="../Agendamentos/Agendamentos.html"></botao-voltar>
+                </div>
+            </div>
             <h2 class="section-title">Agendar Nova Consulta</h2>
             
             <div class="row">
